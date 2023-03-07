b09339b9e44208cdbb0e2cdb1e2bacd017fdd86a
iMikhaylichenko <igor.c.m@ukr.net>
2023-03-07 22:03:02 +0100

    prod

--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-cd /home/sport-be || exit 1
-
-git pull
-
 docker-compose stop
 docker image prune -a -f
 docker-compose -f docker-compose.yml --env-file .env up --build --remove-orphans -d
