b5f98fece2fd709abae788311769bf2c332193d4
iMikhaylichenko <igor.c.m@ukr.net>
2023-03-07 21:56:00 +0100

    port

--- main.py
+++ main.py
@@ -36,4 +36,4 @@ async def shutdown() -> None:
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", port=8080, host="0.0.0.0", reload=True, env_file=".env.dev")
+    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True, env_file=".env.dev")
